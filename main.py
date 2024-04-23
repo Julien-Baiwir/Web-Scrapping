331c300ed0d7ef2c930530ccec913f24a2a75f5c
Julien-Baiwir <julien.freddy.baiwir@gmail.com>
2024-04-23 11:54:34 +0200

    soup set up

---
+++ main.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+
+with open('index.html','r') as html_file:
+     content = html_file.read()
+    #  print(content)
+     soup = BeautifulSoup(content, 'lxml')
+     print(soup.prettify())
